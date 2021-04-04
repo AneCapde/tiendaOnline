a19c2f0604eda6c43b66918be15bb0352c97bfeb
AneCapde <anecapdevila@gmail.com>
2021-04-05 00:00:16 +0200

    NO CAMBIARpackages de main. el server se lanza ahi

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -2,11 +2,11 @@ package main;
 
 
 
-public class main {
+public class Main {
 	public static final String NOMBRE_BD = "tiendaonline.bd";
 	public static final String NOMBRE_BD_TEST = "multiJuegosTest.bd";
 	
-	public main() {
+	public Main() {
 		//Connection con = BD.initBD(NOMBRE_BD);
 		//Statement st = BD.usarCrearTablasBD(con);
 	}
