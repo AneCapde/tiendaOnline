3a84b6f6d7d14f58cf264b548c9c410d265c6e5e
AneCapde <anecapdevila@gmail.com>
2021-04-06 20:21:02 +0200

    clases del server

---
+++ src/main/java/es/deusto/spq/server/ClientesServer.java
@@ -0,0 +1,42 @@
+package es.deusto.spq.server;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import dao.DBManager;
+import models.Cliente;
+
+@Path("/clientes")
+public class ClientesServer {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Cliente> getClientes() {
+		List<Cliente> clientes = DBManager.getInstance().getClientes();
+		return clientes;
+    }
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addCliente(Cliente cliente) {
+        DBManager.getInstance().store(cliente);
+    }
+    @DELETE
+    @Path("/{code}")
+    public Response deleteUser(@PathParam("code") int code) {
+        if (code == 10) {
+            System.out.println("Deleting cliente...");
+            return Response.status(Response.Status.OK).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+}
---
+++ src/main/java/es/deusto/spq/server/ProductosServer.java
@@ -0,0 +1,42 @@
+package es.deusto.spq.server;
+
+import java.util.List;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import dao.DBManager;
+import models.Producto;
+
+@Path("/productos")
+public class ProductosServer {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Producto> getProductos() {
+		List<Producto> productos = DBManager.getInstance().getProductos();
+		return productos;
+    }
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void addProducto(Producto producto) {
+        DBManager.getInstance().store(producto);
+    }
+    @DELETE
+    @Path("/{code}")
+    public Response deleteUser(@PathParam("code") int code) {
+        if (code == 10) {
+            System.out.println("Deleting produtco...");
+            return Response.status(Response.Status.OK).build();
+        } else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+}
