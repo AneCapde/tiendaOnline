565a2a02b2099d0e7abb844ec8d3094ca9e28ea6
Mikel <mikelfuentes@opendeusto.es>
2021-04-21 12:39:34 +0200

    Inicio de test unitario para categorias

---
+++ src/test/java/es/deusto/spq/models/CategoriaTest.java
@@ -0,0 +1,48 @@
+package es.deusto.spq.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import es.deusto.spq.models.CategoriaTest;
+
+public class CategoriaTest {
+
+	
+	@Before
+	public void initiali(){
+		Categoria cat = new Categoria("Nombre", "Descripcion");
+	}
+	
+
+	@Test
+	public void testGetNombre() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetNombre() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescription() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDescription() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testToStringDebug() {
+		//fail("Not yet implemented");
+	}
+
+	@Test
+	public void testTostring() {
+		//fail("Not yet implemented");
+	}
+
+}
