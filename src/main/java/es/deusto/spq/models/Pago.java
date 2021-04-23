c05dc06a03abe5ff16308eaccd9c10f4b2da1a48
gorkalbizu <gorkalbizu@opendeusto.es>
2021-04-23 11:17:49 +0200

    Ventanas US Metodo de Pago

---
+++ src/main/java/es/deusto/spq/models/Pago.java
@@ -0,0 +1,12 @@
+package es.deusto.spq.models;
+
+import java.util.HashMap;
+
+public class Pago {
+    
+    private String metodo;
+    private HashMap<String,String> credenciales = new HashMap<String,String>();
+
+    
+
+}
