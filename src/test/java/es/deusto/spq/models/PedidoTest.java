7565754fa95dd9c7717a05c68396e44c779c36fe
Mikel <mikelfuentes@opendeusto.es>
2021-04-25 12:04:56 +0200

    Test de pedido

--- src/test/java/es/deusto/spq/models/PedidoTest.java
+++ src/test/java/es/deusto/spq/models/PedidoTest.java
@@ -2,82 +2,119 @@ package es.deusto.spq.models;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
+import javax.print.attribute.standard.PDLOverrideSupported;
+
 import org.junit.Before;
 import org.junit.Test;
 
+import es.deusto.spq.models.Cliente.Genero;
+
 public class PedidoTest {
 	
-//	Pedido ped;
-//	Cliente cli;
-//	@Before
-//	public void testPedido() {
-//		cli = new Cliente(DNI, nombre, apellidos, email, password, telefono, direccion, genero, cod_postal, provincia, localidad)
-//		ped = new Pedido(cliente, fecha, estado, importe, cantidad, producto)
-//	}
-//
-//	@Test
-//	public void testGetCliente() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetCliente() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetFecha() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetFecha() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetEstado() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetEstado() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetImporte() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetImporte() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetCantidad() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetCantidad() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testGetProducto() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testSetProducto() {
-//		fail("Not yet implemented");
-//	}
-//
-//	@Test
-//	public void testToString() {
-//		fail("Not yet implemented");
-//	}
+	Pedido ped;
+	Cliente cli;
+	Producto pro;
+	
+	@Before
+	public void testPedido() {
+		cli = new Cliente("12345F", "NomCliente", "ApCliente", "email", "pass", 9999999,"Direccion", Genero.HOMBRE, 234455, "provincia", "localidad");
+		Categoria cat = new Categoria("nombreCat", "descCat");
+		SubCategoria subca = new SubCategoria("nombreSub", "descSub", cat);
+		Marca marc = new Marca("nombreMarc", "descMarc");
+		pro = new Producto("producto", "descripcion", 2, 2, "img", subca, marc);
+		ped = new Pedido(cli, new Date(1619342158), "estado",22 ,2, pro);
+	}
+
+	@Test
+	public void testGetCliente() {
+		Cliente resul = ped.getCliente();
+		assertEquals(cli, resul);
+	}
+
+	@Test
+	public void testSetCliente() {
+		
+		
+		Cliente resul = ped.getCliente();
+		assertEquals(cli, resul);
+	}
+
+	@Test
+	public void testGetFecha() {
+		Date resul = ped.getFecha();
+		assertEquals(new Date(1619342158), resul);
+	}
+
+	@Test
+	public void testSetFecha() {
+		ped.setFecha(new Date(1619342157));
+		Date resul = ped.getFecha();
+		assertEquals(new Date(1619342157), resul);
+	}
+
+	@Test
+	public void testGetEstado() {
+		String resul = ped.getEstado();
+		assertEquals("estado", resul);
+	}
+
+	@Test
+	public void testSetEstado() {
+		ped.setEstado("estado2");
+		String resul = ped.getEstado();
+		assertEquals("estado2", resul);
+	}
+
+	@Test
+	public void testGetImporte() {
+		int resul = ped.getImporte();
+		assertEquals(22, resul);
+	}
+
+	@Test
+	public void testSetImporte() {
+		ped.setImporte(23);
+		int resul = ped.getImporte();
+		assertEquals(23, resul);
+	}
+
+	@Test
+	public void testGetCantidad() {
+		int resul = ped.getCantidad();
+		assertEquals(2, resul);
+	}
+
+	@Test
+	public void testSetCantidad() {
+		ped.setCantidad(4);
+		int resul = ped.getCantidad();
+		assertEquals(4, resul);
+	}
+
+	@Test
+	public void testGetProducto() {
+		Producto resul = ped.getProducto();
+		assertEquals(pro, resul);
+	}
+
+	@Test
+	public void testSetProducto() {
+		cli = new Cliente("12345F", "NomCliente", "ApCliente", "email", "pass", 9999999,"Direccion", Genero.HOMBRE, 234455, "provincia", "localidad");
+		Categoria cat = new Categoria("nombreCat", "descCat");
+		SubCategoria subca = new SubCategoria("nombreSub", "descSub", cat);
+		Marca marc = new Marca("nombreMarc", "descMarc");
+		Producto pron = new Producto("producto2", "descripcion2", 2, 2, "img", subca, marc);
+		ped.setProducto(pron);
+		Producto resul = ped.getProducto();
+		assertEquals(pron, resul);
+	}
+
+	@Test
+	public void testToString() {
+		String resul = ped.toString();
+		assertEquals("Pedido [fecha=Mon Jan 19 18:49:02 CET 1970, estado=estado, importe=22, cantidad=2, cliente=es.deusto.spq.models.Cliente@5fcfe4b2, producto=producto]", resul);	
+	}
 
 }
