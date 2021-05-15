abfd8530d504c9e7c5e3f3d39986c634861feb10
Ainaraast <ainara.astondoa@opendeusto.es>
2021-05-15 17:13:43 +0200

    Test de integración Pedido

--- src/test/java/es/deusto/spq/preformingTest/TiendaOnlineTest.java
+++ src/test/java/es/deusto/spq/preformingTest/TiendaOnlineTest.java
@@ -1,10 +1,12 @@
 package es.deusto.spq.preformingTest;
 
+import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.MediaType;
@@ -32,8 +34,11 @@ public class TiendaOnlineTest {
     static Logger logger = Logger.getLogger(TiendaGUI.class.getName());
     private HttpServer server;
     private WebTarget target;
-
-	// If you use the EmptyReportModule, the report is not generated
+    Cliente cliente;
+    List<Categoria> categorias;
+    List<SubCategoria> subCategorias;
+    
+    // If you use the EmptyReportModule, the report is not generated
 	//@Rule public ContiPerfRule rule = new ContiPerfRule(new EmptyReportModule());
 	@Rule 
     public ContiPerfRule rule = new ContiPerfRule();
@@ -57,7 +62,7 @@ public class TiendaOnlineTest {
         Client client = ClientBuilder.newClient();
         final WebTarget appTarget = client.target("http://localhost:8080/myapp");
         final WebTarget clientesTarget = appTarget.path("/clientes").path("/"+"usuario@gmail.com").path("/"+ "12345r");
-		Cliente cliente = clientesTarget.request(MediaType.APPLICATION_JSON).get(Cliente.class);
+		cliente = clientesTarget.request(MediaType.APPLICATION_JSON).get(Cliente.class);
     }
     
     @Test 
@@ -68,7 +73,7 @@ public class TiendaOnlineTest {
         final WebTarget appTarget = client.target("http://localhost:8080/myapp");
 		final WebTarget categoriasTarget = appTarget.path("/categorias");
 		GenericType<List<Categoria>> genericType_categoria = new GenericType<List<Categoria>>() {};
-        List<Categoria> categorias = categoriasTarget.request(MediaType.APPLICATION_JSON).get(genericType_categoria);
+        categorias = categoriasTarget.request(MediaType.APPLICATION_JSON).get(genericType_categoria);
     }
     
     @Test 
@@ -79,21 +84,22 @@ public class TiendaOnlineTest {
         final WebTarget appTarget = client.target("http://localhost:8080/myapp");
         final WebTarget subTarget = appTarget.path("/subcategorias");
 		GenericType<List<SubCategoria>> genericType_sub = new GenericType<List<SubCategoria>>() {};
-		List<SubCategoria> subCategorias = subTarget.request(MediaType.APPLICATION_JSON).get(genericType_sub);
+		subCategorias = subTarget.request(MediaType.APPLICATION_JSON).get(genericType_sub);
     }
     
-//    @Test 
-//    @PerfTest(invocations = 1000, threads = 20)
-//    @Required(max = 20000, average = 3000)
-//    public void connectionPedido(){
-//        Client client = ClientBuilder.newClient();
-//        final WebTarget appTarget = client.target("http://localhost:8080/myapp");
-//        final WebTarget pedTarget = appTarget.path("/pedidos").path("/"+"12399345K");
-//		  GenericType<List<Pedido>> genericType_ped = new GenericType<List<Pedido>>() {};
-//		  List<Pedido> pedidos = pedTarget.request(MediaType.APPLICATION_JSON).get(genericType_ped);
-//        
-//    }
-//    
+    @Test 
+    @PerfTest(invocations = 1000, threads = 20)
+    @Required(max = 20000, average = 3000)
+    public void connectionPedido(){
+    	Date date = new Date();
+        Client client = ClientBuilder.newClient();
+        final WebTarget appTarget = client.target("http://localhost:8080/myapp");
+        final WebTarget pedidoTarget = appTarget.path("/pedidos");
+        Pedido pedido = new Pedido(cliente, date, "en proceso", 10, 1, "A Coruña");
+        pedidoTarget.request(MediaType.APPLICATION_JSON).post(Entity.entity(pedido, MediaType.APPLICATION_JSON));
+        
+    }
+    
     
 
 
