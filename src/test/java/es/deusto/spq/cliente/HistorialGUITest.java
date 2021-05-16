8bf7da159a5da0a4f002d28777ed655f67065302
Ainaraast <ainara.astondoa@opendeusto.es>
2021-05-16 17:07:12 +0200

    Cambios en producto para los comentarios

--- src/test/java/es/deusto/spq/cliente/HistorialGUITest.java
+++ src/test/java/es/deusto/spq/cliente/HistorialGUITest.java
@@ -10,16 +10,23 @@
 //
 //import org.junit.Before;
 //import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.mockito.Mock;
+//import org.mockito.MockedStatic;
 //import org.mockito.Mockito;
+//import org.mockito.junit.MockitoJUnitRunner;
 //
-//import es.deusto.spq.models.Pedido;
-//import es.deusto.spq.models.Producto;
 //import es.deusto.spq.models.Cliente;
 //import es.deusto.spq.models.Cliente.Genero;
+//import es.deusto.spq.models.Pedido;
 //
+//@RunWith(MockitoJUnitRunner.class)
 //public class HistorialGUITest {
+//	
 //	HistorialGUI historialGUI;
 //	WebTarget web = Mockito.mock(WebTarget.class);
+//	
+//	@Mock
 //	TiendaGUI tiendaGUI = Mockito.mock(TiendaGUI.class);
 //
 //	List<Pedido> pedidos = new ArrayList<Pedido>();
@@ -30,8 +37,9 @@
 //		Cliente cli = new Cliente("12399345K", "usuario", "apellido apellido ", "usuario@gmail.com", "12345r", 655786943,
 //				"direccion", Genero.MUJER, 45678, "provincia", "localidad");
 //		
-//		pedido = new Pedido(cli,new Date(1619342158), "estado",22 ,2);
+//		pedido = new Pedido(cli,new Date(1619342158), "estado",22 ,2,"lugar");
 //		pedidos.add(pedido);
+//	
 //		historialGUI = new HistorialGUI(tiendaGUI,web);
 //		
 //	}
