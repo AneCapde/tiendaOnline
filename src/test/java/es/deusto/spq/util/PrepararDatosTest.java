3d98227603b462bbfda509fb6eb65710b9f16ca7
AneCapde <ane.capdevila@opendeusto.es>
2021-05-20 09:08:22 +0200

    test idomas

--- src/test/java/es/deusto/spq/util/PrepararDatosTest.java
+++ src/test/java/es/deusto/spq/util/PrepararDatosTest.java
@@ -13,29 +13,27 @@ public class PrepararDatosTest{
 
 @Before 
 public void setUp() {
-    PrepararDatos.getInstance().getEspanyol().put("nombre1", "Español");
-    PrepararDatos.getInstance().getEspanyol().put("nombre2", "Ingles");
-
-    PrepararDatos.getInstance().getIngles().put("nombre1", "English");
-    PrepararDatos.getInstance().getIngles().put("nombre2", "Spanish");
+}
+@Test
+public void testPerpararDatosXML(){
+    assertEquals("English", PrepararDatos.getInstance().getIngles().get("nombre1"));
 }
 @Test
 public void testGetEspanyol(){
     HashMap<String, String> idioma_esp_1 = new HashMap<>();
     idioma_esp_1.put("nombre1", "Español");
     idioma_esp_1.put("nombre2", "Ingles");
-    assertEquals(idioma_esp_1, PrepararDatos.getInstance().getEspanyol());
+    assertEquals(idioma_esp_1.get("nombre1"), PrepararDatos.getInstance().getEspanyol().get("nombre1"));
 }
 @Test
 public void testGetIngles(){
     HashMap<String, String> idioma_en_1 = new HashMap<>();
     idioma_en_1.put("nombre1", "English");
     idioma_en_1.put("nombre2", "Spanish");
-    assertEquals(idioma_en_1, PrepararDatos.getInstance().getIngles());
+    assertEquals(idioma_en_1.get("nombre1"), PrepararDatos.getInstance().getIngles().get("nombre1"));
 }
 @Test
-public void testGetPalabraEspanyol(){
-    
+public void testGetPalabraEspanyol(){   
     assertEquals("Español", PrepararDatos.getInstance().getPalabraEspanyol("nombre1"));
 }
 @Test
